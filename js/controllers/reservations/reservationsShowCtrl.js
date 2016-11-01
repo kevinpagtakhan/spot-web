5d07f5746f592816ff24294a9f8260c008027f3e
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-11-01 15:36:43 -0700

    Reservation show view with map

--- js/controllers/reservations/reservationsShowCtrl.js
+++ js/controllers/reservations/reservationsShowCtrl.js
@@ -1,28 +1,36 @@
 angular.module('app')
   .controller('ReservationsShowController', ReservationsShowController);
 
-ReservationsShowController.$inject = ['$stateParams', 'ReservationsFactory'];
+ReservationsShowController.$inject = ['$state', '$stateParams', 'ReservationsFactory'];
 
-function ReservationsShowController($stateParams, ReservationsFactory){
+function ReservationsShowController($state, $stateParams, ReservationsFactory){
   var vm = this;
   vm.isLoading = true;
+  vm.address = {}
+  vm.map = {}
 
   vm.updateMap = function(){
-    if(!vm.address.origin){
-      vm.map.src = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyBxDN6q_NrQougCGoZU22AsMrWNnri82u0&q=Santa+Monica+CA'
-    }
+    var address = vm.reservation._space.address;
+    vm.address.origin = address.line_1 + ' ';
+    vm.address.origin += address.line_2 + ' ';
+    vm.address.origin += address.city + ' ';
+    vm.address.origin += address.state + ' ';
+    vm.address.origin += address.zip_code + ' ';
+
+    if(vm.address.origin){
+      vm.map.src = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyBxDN6q_NrQougCGoZU22AsMrWNnri82u0&q=' + vm.address.origin;
 
       document.getElementById('mymap').setAttribute('src', vm.map.src);
+    }
   }
-
-  vm.updateMap();
 
   ReservationsFactory.show($stateParams.id)
     .then(function success(data){
       console.log(data);
       vm.reservation = data.data.data;
+      vm.updateMap();
       vm.isLoading = false;
     }, function failure(data){
-      console.log(data);
+      $state.go('logout');
     });
 }
