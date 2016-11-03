28157ff186e2f7237acb34236c5b847da3445165
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-11-03 07:00:38 -0700

    Add loader while waiting for server response

--- js/controllers/registerCtrl.js
+++ js/controllers/registerCtrl.js
@@ -5,13 +5,15 @@ RegisterController.$inject = ['$http', '$state'];
 
 function RegisterController($http, $state) {
   var vm = this;
+  vm.isLoading = false;
 
   vm.createUser = function(){
     var apiURL = 'http://localhost:3000/api/';
-
+    vm.isLoading = true;
     $http.post(apiURL + 'users/', vm.newUser)
       .then(function (data) {
         if(data.data.success){
+          vm.isLoading = false;
           $state.go('login');
         } else {
           vm.user.password = '';
